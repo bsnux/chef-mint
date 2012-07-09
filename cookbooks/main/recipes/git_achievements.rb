b49f2b73a68f47fe88e0d2eb91dc5eaa688752f3
Arturo Fernandez <arturo@bsnux.com>
2012-07-10 00:02:23 +0200

    Github recipe working

--- cookbooks/main/recipes/git_achievements.rb
+++ cookbooks/main/recipes/git_achievements.rb
@@ -1,14 +1,1 @@
-git_url = 'git@github.com:bsnux/'
-dev_dir = '/tmp/'
-repos = ['vim-kit', 'django-bootstrap-scripts', 'git-achievements']
 
-repos.each do |repo|
-  git 'Clone repo' do
-    user 'bsnux'
-    group 'bsnux'
-    repository git_url + repo
-    reference 'master'
-    action :checkout
-    destination dev_dir + repo
-  end
-end
--- cookbooks/main/recipes/github_bsnux.rb
+++ cookbooks/main/recipes/github_bsnux.rb
@@ -1,6 +1,16 @@
 git_url = 'git@github.com:bsnux/'
-dev_dir = '/tmp/'
-repos = ['linux-config', 'vim-kit', 'django-bootstrap-scripts']
+dev_dir = '/home/bsnux/dev/'
+repos = ['vim-kit', 'django-bootstrap-scripts', 'git-achievements']
+
+execute 'copy SSH public key' do
+  command 'cp /home/bsnux/.ssh/id_rsa.pub /root/.ssh/'
+  action :run
+end
+
+execute 'copy SSH private key' do
+  command 'cp /home/bsnux/.ssh/id_rsa /root/.ssh/'
+  action :run
+end
 
 repos.each do |repo|
   git 'Clone repo' do
