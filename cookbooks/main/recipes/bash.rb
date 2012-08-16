2a444a568abc8440a241125848a2dd61193bf7df
Manuel Ignacio Franco <manuel@manuel-HP-Compaq-dc7800-Small-Form-Factor.(none)>
2012-08-16 10:46:10 +0200

    user defined by configuration param at solo.json

--- cookbooks/main/recipes/bash.rb
+++ cookbooks/main/recipes/bash.rb
@@ -1,6 +1,6 @@
-template '/home/bsnux/.bashrc' do
+template node['user']['default']['home_directory'] + '.bashrc' do
   source 'bash.erb'
   mode '644'
-  owner 'bsnux'
-  group 'bsnux'
+  owner node['user']['default']['username']
+  group node['user']['default']['username']
 end
--- cookbooks/main/recipes/github_bsnux.rb
+++ cookbooks/main/recipes/github_bsnux.rb
@@ -1,21 +1,21 @@
 git_url = 'git@github.com:bsnux/'
-dev_dir = '/home/bsnux/dev/'
+dev_dir = node['user']['default']['home_directory'] + 'dev/'
 repos = ['vim-kit', 'django-bootstrap-scripts', 'git-achievements']
 
 execute 'copy SSH public key' do
-  command 'cp /home/bsnux/.ssh/id_rsa.pub /root/.ssh/'
+  command 'cp ' + node['user']['default']['home_directory'] + '.ssh/id_rsa.pub /root/.ssh/'
   action :run
 end
 
 execute 'copy SSH private key' do
-  command 'cp /home/bsnux/.ssh/id_rsa /root/.ssh/'
+  command 'cp ' + node['user']['default']['home_directory'] + '.ssh/id_rsa /root/.ssh/'
   action :run
 end
 
 repos.each do |repo|
   git 'Clone repo' do
-    user 'bsnux'
-    group 'bsnux'
+    user node['user']['default']['username']
+    group node['user']['default']['username']
     repository git_url + repo
     action :checkout
     destination dev_dir + repo
